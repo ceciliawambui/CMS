35ee638eb895a52ed804c5b0549c64a278c4874e
ceciliawambui <ceciliawambui026@gmail.com>
2022-02-11 11:47:14 +0300

    Fix: Styles

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -23,9 +23,10 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        if($this->app->environment('production')) {
-            \URL::forceScheme('https');
-                    }
+        if (env('APP_ENV') != 'local') {
+            URL::forceScheme('https');
+        } 
+       
         //
     }
 }
